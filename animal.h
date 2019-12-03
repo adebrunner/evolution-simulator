093b6bc5d0f9d433f80a602cfd7d4128600cd85c
Isaac Mathers <isaactm98@gmail.com>
2019-12-03 17:49:44 -0500

    Added memory location of animal in world to animal class.

--- animal.h
+++ animal.h
@@ -4,23 +4,23 @@
 class Animal
 {
 public:
-    Animal(int s=1, int sp=1, int e=1, int hl = 0)
+    Animal(int s=1, int sp=1, int e=1, int l = 0)
     {
         setStrength(s);
         setSpeed(sp);
         setEnergy(e);
-        setHomeLocation(hl);
+        setLocation(l);
     }
 
     void setStrength(int strength) { this->strength = strength; }
     void setSpeed(int speed) { this->speed = speed; }
     void setEnergy(int energy) { this->energy = energy; }
-    void setHomeLocation(int hl) { this->home_location = hl; }
+    void setLocation(int l) { this->location = l; }
 
 	int getStrength() { return this->strength; }
 	int getSpeed() { return this->speed; }
 	int getEnergy() { return this->energy; }
-    int getHomeLocation() { return this->home_location; }
+    int getLocation() { return this->location; }
 
 
 private:
@@ -28,6 +28,7 @@ private:
     int speed;         // Determines how many blocks an animal can move per unit of energy
     int energy;        // Determines how many moves an animal can make. Should be inversely proportional
                        // to strength
+    int location;      // Memory index of world of current animal position.
 };
 
 #endif
