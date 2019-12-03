f2bf7ec63bf29bc86a9645e5924ddb283436e4d9
adebrunner <debrunac@mail.uc.edu>
2019-12-03 17:12:23 -0500

    Changes world to include extra spaces around border for houses

--- world.h
+++ world.h
@@ -13,25 +13,37 @@ public:
     {
         setFood(f);
         setNumAnimals(a);
-        setDim(d);
+        setPlayableDim(d);
+		setHouseDim(d + 2);
 
-		// Initialize the board with open spaces
-        this->board = (Space*)malloc(sizeof(Space)*this->dim*this->dim);
-        for (int i = 0; i < d*d; i++) {
+		// Initialize the board with open spaces. Allocate d+2 to have border for homes
+        this->board = (Space*)malloc(sizeof(Space)*getHouseBoardSize());
+        for (int i = 0; i < getHouseBoardSize(); i++) {
 			Space space;
+
+			// Make border spaces houses
+			int row = i / getHouseDim();
+			int col = i % getHouseDim();
+			if (row == 0 || row == (getHouseDim() - 1) || col == 0 || col == (getHouseDim() - 1)) {
+				space.makeHome();
+			}
+
             *(this->board + i) = space;
         }
     }
 
     void setFood(int food) { this->food = food; }
     void setNumAnimals(int num_animals) { this->num_animals = num_animals; }
-    void setDim(int dim) { this->dim = dim; }
+    void setPlayableDim(int dim) { this->playable_dim = dim; }
+	void setHouseDim(int dim) { this->house_dim = dim; }
 
     Space* getBoard() { return this->board; }
 	int getFood() { return this->food; }
 	int getNumAnimals() { return this->num_animals; }
-	int getDim() { return this->dim; }
-	int getBoardSize() { return this->dim*this->dim; }
+	int getPlayableDim() { return this->playable_dim; }
+	int getHouseDim() { return this->house_dim; }
+	int getPlayableBoardSize() { return this->playable_dim*this->playable_dim; }
+	int getHouseBoardSize() { return this->house_dim*this->house_dim; }
 
     // Place food in random spaces on board
     void populateFood() { 
@@ -39,8 +51,10 @@ public:
         srand(time(NULL));
         int numFood = 0;
         while(numFood < this->food) {
-            int index = rand() % this->getBoardSize();
-            if ((*(this->board + index)).getContainsFood() == false) {
+            int index = rand() % this->getHouseBoardSize();
+
+			// Put food at any open space that is not a house
+            if (!(*(this->board + index)).getIsHome() && (*(this->board + index)).getContainsFood() == false) {
                 (*(this->board + index)).putFood();
                 numFood += 1;
             }
@@ -50,7 +64,8 @@ public:
 private:
     int food; // Determines the amount of spaces that contain food
     int num_animals; // Determines the starting number of animals
-    int dim; // Determines the dimension of the world; world will be dim*dim spaces
+	int house_dim; // Dim of world including houses
+    int playable_dim; // Dim of world not including houses
     Space* board;
 
 };
