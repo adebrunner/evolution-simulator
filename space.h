d8ef38312f4c21ece479fe38b1361471135cf3ce
Isaac Mathers <isaactm98@gmail.com>
2019-12-04 16:56:18 -0500

    Began adding atomic support to space class.

--- space.h
+++ space.h
@@ -1,19 +1,41 @@
 #ifndef SPACE_H
 #define SPACE_H
 
-class Space 
+#include <atomic>
+
+using std::atomic;
+
+class Space
 {
 public:
-    Space(bool f = false, bool a = false, bool h = false) 
+    Space(bool f = false, bool a = false, bool h = false)
     {
         setContainsFood(f);
         setContainsAnimal(a);
         setIsHome(h);
     }
 
+    /// Copy constructor that supports moving atomic variables.
+    /// Note, this should only be done by one thread as our memory order is relaxed.
+    Space(const Space& s):
+            contains_animal(s.contains_animal.load()),
+            contains_food(s.contains_food.load())
+    {
+        this->is_home = s.is_home;
+    }
+
+    /// Needed to overload this due to atomic variables.
+    Space operator=(const Space& s)
+    {
+        this->contains_animal.store(s.contains_animal.load());
+        this->contains_food.store(s.contains_food.load());
+        this->is_home = s.is_home;
+        return this;
+    }
+
     // TODO, perhaps we can remove these. Should just use setters below imo.
     void putFood() { this->contains_food = true; }
-	bool putAnimal() { this->contains_animal = true; return true; }
+    bool putAnimal() { this->contains_animal = true; return true; }
     void makeHome() { this->is_home = true; }
 
     void setContainsFood(bool contains_food) { this->contains_food = contains_food; }
@@ -26,8 +48,8 @@ public:
 
 
 private:
-    bool contains_food;
-    bool contains_animal;
+    atomic<bool> contains_food;
+    atomic<bool> contains_animal;
     bool is_home;
 };
 
